1762601f582820bdd0f0847c5ba9a86de8d9bafb
TonyGr22 <tony-gr@outlook.es>
2017-07-05 23:41:45 -0500

    Clases

---
+++ Servi_fiestas_Alfredos/filosofia.php
@@ -0,0 +1,66 @@
+
+<br>
+<br>
+<br>
+<div class="container">
+    <br>
+    <br>
+    <center>
+        <div class="item active" width="1000" height="300" class="col-xs-4 col-sm-12">
+
+            <img src="<?php echo base_url(); ?>/images/banner_filosofia.jpg" class="img-responsive" alt=""> 
+        </div>
+        <br>
+        <br>
+        
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 well" >
+
+            <h2>Misión</h2>
+            Brindar un servicio integral creando banquetes de la más alta calidad de materias<br> con personal con inagotable espíritu de servicio para sus eventos sociales dentro y fuera de la ciudad.<br>
+
+        </div>
+        <div class="col-xs-12 col-sm-6 well">
+
+            <h2>Visión</h2>
+            Ser una empresa de Alto prestigio, por su constante innovación en las tendencias gastronómicas, ofreciendo un servicio que satisfaga las más altas exigencias a nuestros clientes de la ciudad de Acámbaro y sus alrededores.
+
+        </div>
+            
+        </div>
+        <div class="well" width="2000" height="500" class="col-xs-4 col-sm-12">
+
+            <h2>Políticas</h2>
+            <td>	Brindar un buen servicio y trato al cliente que lo satisfaga en todos los aspectos.</td>
+            <br>
+            <td>	Elaborar los platillos con ingredientes de la mejor calidad.</td>
+            <br>
+            <td>	Ofrecer al cliente diferentes opciones de platillos. </td>
+            <br>
+            <td>	Contar con la infraestructura adecuada.</td>
+            <br>
+            <td>	Llevar a cabo un cuidadoso proceso de higiene en todas las áreas de la empresa.</td>
+            <br>
+            <td>	Proporcionar al cliente un producto sano, de excelente sabor y nutritivo.</td>
+            <br>
+            <td>	Brindar trato justo y esmerado a todos los clientes.</td>
+            <br>
+            <td>	Todos los integrantes de la empresa deben mantener un comportamiento ético.</td>
+            <br>
+            <td>	Todas las personas que renten mobiliario y lo entreguen incompleto o en mal estado, estarán de acuerdo en pagar una cierta cantidad del mismo.</td>
+            <br>
+            <td>	Brindar trato justo y esmerado a todos los clientes, en sus solicitudes y reclamos considerando que el fin de la empresa es el servicio a la comunidad.</td>
+            <br>
+            <td>	Solo el Director General puede autorizar plazos de crédito a clientes superiores a 10 días.</td>
+            <br>
+            <td></td>
+        </div>
+        <div class="well">
+            <h2>Valores:</h2>
+            	Respeto
+            <br>	Honestidad
+            <br>	Humildad
+        </div>
+
+    </center>
+</div>
---
+++ Servi_fiestas_Alfredos/productos.php
@@ -0,0 +1,67 @@
+<br>
+<div class="container">
+
+    <!--Primer fila -->
+
+    <div class="row">
+        <br>
+        <center><h1> Animaciones HTML5 + CSS3 </h1></center>
+        <div class="col-xs-12 col-sm-4">
+
+            <h3> Sillas </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/silla.png" class="img-responsive" alt=""> 
+            <br>
+            <input type="submit">
+        </div>
+
+        <div class="col-xs-12 col-sm-4" >
+
+            <h3> Mesa Redonda </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/mesa.jpg" width="200" height="220" class="anim_rotate" >
+            <br>
+            <input type="submit">
+        </div>        
+        <div class="col-xs-12 col-sm-4">
+
+            <h3> Mesa Larga </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/mesal.jpg" width="200" height="220" class="anim_scale_rotate">
+            <br>
+            <input type="submit">
+        </div>
+
+    </div>
+
+    <!--Segunda fila -->
+
+    <div class="row">
+
+        <div class="col-xs-12 col-sm-4" style="perspective: 600px;">
+
+            <h3> Toldo 10x10 </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/toldo.png" width="200" height="220" class="anim_rotate_y">
+            <br>
+            <input type="submit">
+        </div>
+
+        <div class="col-xs-12 col-sm-4" style="perspective: 600px;">
+
+            <h3> Inflable </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/inflable.png" width="200" height="220" class="anim_rotate_x">
+            <br>
+            <input type="submit">
+        </div>
+
+        <div class="col-xs-12 col-sm-4">
+
+            <h3> Meseros 8 Hrs </h3>
+            <img src="<?php echo base_url(); ?>/images/productos/mesero.png" width="200" height="220" class="opacity">
+            <br>
+            <input type="submit">
+        </div>
+
+    </div>
+
+
+
+</div>
+<br>
