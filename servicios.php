3ae7c5556a1eaeee11dd5fce1cc702df4a54240c
TonyGr22 <tony-gr@outlook.es>
2017-07-08 23:37:34 -0500

    Servicios

---
+++ servicios.php
@@ -0,0 +1,97 @@
+<br> 
+<section id="services" class="service-item">
+    <div class="container">
+        <div class="center wow fadeInDown">
+            <h2>Nuestros Servicios</h2>
+            <p class="lead">En Servifiestas Alfredo's tenemos todo para tu evento social <br> Un servicio de calidad y sobre todo al mejor precio</p>
+        </div>
+
+        <div class="row">
+
+            <div class="col-sm-6 col-md-4" width="200" height="220">
+                <div class="media services-wrap wow fadeInDown">
+                    <div class="pull-left">
+                        <img class="img-responsive" src="../../images/services/services1.jpg" >
+                    </div>
+                    <div class="media-body">
+                        <h3 class="media-heading">Banquetes</h3>
+                        <p>Servicio de banquetes para todo tipo de  evento social</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4">
+                <div class="media services-wrap wow fadeInDown">
+                    <div class="pull-left">
+                        <img class="img-responsive" src="../../images/services/services2.jpg" width="200" height="220">
+                    </div>
+                    <div class="media-body">
+                        <h3 class="media-heading">Alquiler de loza y cristalería</h3>
+                        <p>Tenemos el más nuevo y actualizado equipo de cristalería disponible para cualquier evento</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4">
+                <div class="media services-wrap wow fadeInDown">
+                    <div class="pull-left">
+                        <img class="img-responsive" src="../../images/services/services3.jpg">
+                    </div>
+                    <div class="media-body">
+                        <h3 class="media-heading">Alquiler de mobiliario</h3>
+                        <p>Tenemos el más nuevo y sofisticado mobiliario esperando a ser alquilado para hacer de tu evento algo especial</p>
+                    </div>
+                </div>
+            </div>  
+                                               
+        </div><!--/.row-->
+    </div><!--/.container-->
+</section><!--/#services-->
+
+
+<section id="feature" class="transparent-bg">
+    <div class="container">
+        <div class="get-started center wow fadeInDown">
+            <h2>List@ Para Comenzar</h2>
+            <p class="lead">Puedes encontrar un gran variedad de platillos en los que podrás elegir <br> Además contamos con servicio de mobiliario</p>
+
+        </div><!--/.get-started-->
+
+        <div class="clients-area center wow fadeInDown">
+            <h2>Que es lo que dicen nuestros clientes</h2>
+            <p class="lead">Aquí podrás ver lo que nuestros clientes piensan de nuestro servicio <br>Dando la mejor atención para resolver todas tus dudas</p>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 wow fadeInDown">
+                <div class="clients-comments text-center">
+                    <img src="<?php echo base_url(); ?>/images/client1.png" class="img-circle" alt="">
+                    <h3>Siempre que necesito ayuda con un evento grande con mi familia siempre acudo a ServiFiestas Alfredo´s</h3>
+                    <h4><span>-José Reyes /</span>  Cliente Frecuente</h4>
+                </div>
+            </div>
+            <div class="col-md-4 wow fadeInDown">
+                <div class="clients-comments text-center">
+                    <img src="<?php echo base_url(); ?>/images/client2.png" class="img-circle" alt="">
+                    <h3>Creamos un convenio para que ServiFiestas Alfredo´s pudiera convertirse en el encargado del comedor de Fantasy corp. </h3>
+                    <h4><span>-Sahn hoinj /</span>  Director de Fantasy corp</h4>
+                </div>
+            </div>
+            <div class="col-md-4 wow fadeInDown">
+                <div class="clients-comments text-center">
+                    <img src="<?php echo base_url(); ?>/images/client3.png" class="img-circle" alt="">
+                    <h3>Siempre para nuestras reuniones de trabajo contratamos los servicios para tener un buen servicio y buena comida despues de una larga jornada laboral. </h3>
+                    <h4><span>-Jorge Santillan /</span>  Asistente de BotMex</h4>
+                </div>
+            </div>
+        </div>
+    </div><!--/.container-->
+</section><!--/#feature-->
+
+
+
+
+
+</body>
+</html>
+
