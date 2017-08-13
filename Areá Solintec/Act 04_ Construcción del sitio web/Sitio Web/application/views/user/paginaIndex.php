224b47bb8a91bdbd2cebf8f5cda0a0746469072f
Emmanuel <solintec.official@gmail.com>
2017-08-13 11:13:41 -0500

    Acomodo

---
+++ Areá Solintec/Act 04_ Construcción del sitio web/Sitio Web/application/views/user/paginaIndex.php
@@ -0,0 +1,132 @@
+<br>
+<br>
+<br>
+<br>
+<br>
+<div class="container">
+    
+    <div class="row">
+
+                <div class="col-xs-12 ">
+
+                    <iframe  scrolling="no" src="<?php echo base_url(); ?>/media/sliderAlfredos/Untitled-2/Untitled-2.html" frameborder="0" style="height:400px; width:100%"></iframe>
+
+                </div>
+
+            </div>
+            <br>
+
+
+
+
+
+
+<section  >
+    <div class="container">
+        <div class="center wow fadeInDown">
+            <h2>Servi-Fiestas Alfredo's</h2>
+            <p class="lead">Solo con nosotros encontrarás una gran veriedad de servicio de alimentos<br> Y un amplio surtido en mobiliario para todo tipo de evento social</p>
+        </div>
+        <div class="container">
+        <div class="row">
+            <div class="features">
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                    <div class="feature-wrap">
+                        <i class="fa fa-dollar"></i>
+                        <h2>Nos adaptamos a tu presupuesto</h2>
+                        <h3>Porque no importa la cantidad, si no la calidad.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                    <div class="feature-wrap">
+                        <i class="fa fa-comments"></i>
+                        <h2>Te Ofrecemos Atención Personalizada</h2>
+                        <h3>Resolveremos todas tus dudas.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+
+                <div class="col-md-4 col-sm-6 wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="600ms">
+                    <div class="feature-wrap">
+                        <i class="fa fa-comments"></i>
+                        <h2>Tus Comentarios serán Escuchados</h2>
+                        <h3>Todos los cometarios sirven de retro alimetación así que no dudes de hacerlos.</h3>
+                    </div>
+                </div><!--/.col-md-4-->
+
+            </div><!--/.services-->
+        </div><!--/.row-->  
+        </div>
+    </div><!--/.container-->
+</section><!--/#feature-->
+<br>
+
+<section id="recent-works">
+    <div class="container" >
+        <div class="center wow fadeInDown">
+            <h2>Trabajos Recientes</h2>
+            <p class="lead">Te mostramos algunos delos eventos que hemos realizado<br> A lo Largo de estos meses los cuales se irán actualizando para mostrar más y más. </p>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="recent-work-wrap">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>/images/pag/even1.jpg" alt="">
+                    <div class="overlay">
+                        <div class="recent-work-inner">
+                            <h3><a href="#">Evento de Boda 2015</a> </h3>
+                            <p>Manteleria, mobiliario y cristaleria. </p>
+                           
+                        </div> 
+                    </div>
+                </div>
+            </div>   
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="recent-work-wrap">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>/images/pag/even2.jpg" alt="">
+                    <div class="overlay">
+                        <div class="recent-work-inner">
+                            <h3><a href="#">Eventoto de Boda 2016 </a></h3>
+                            <p>Evento minimalista con servivio de platillos.</p>
+                            
+                        </div> 
+                    </div>
+                </div>
+            </div> 
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="recent-work-wrap">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>/images/pag/even3.png" alt="">
+                    <div class="overlay">
+                        <div class="recent-work-inner">
+                            <h3><a href="#">Servicio de Bocadillos</a></h3>
+                            <p>Servicio de Bocadillos para reunion de trabajo.</p>
+                            
+                        </div> 
+                    </div>
+                </div>
+            </div>   
+
+            <div class="col-xs-12 col-sm-4 col-md-3">
+                <div class="recent-work-wrap">
+                    <img class="img-responsive" src="<?php echo base_url(); ?>/images/pag/even4.jpg"  alt="">
+                    <div class="overlay">
+                        <div class="recent-work-inner">
+                            <h3><a href="#">Evento de 3 años</a></h3>
+                            <p>Servivio de Postres.</p>
+                           
+                        </div> 
+                    </div>
+                </div>
+            </div>   
+ 
+        </div><!--/.row-->
+    </div><!--/.container-->
+</section><!--/#recent-works-->	
+
+</div>
+<br>
+
+
+
